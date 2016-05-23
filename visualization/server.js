98aff79676171a9153efcdefd41e8f27235a22fc
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2016-05-24 02:19:54 +1000

    added request for DB data

--- visualization/server.js
+++ visualization/server.js
@@ -2,8 +2,17 @@
 
 const http = require('http'),
       fs = require('fs'),
+      r = require('rethinkdb'),
       PORT = 80,
       server = http.createServer(handle);
+
+r.connect({host:'rethinkdb'}).then(startServer);
+
+function startServer(conn) {
+    server._rConn = conn;
+    console.log('Connected to DB...');
+    server.listen(PORT, () => console.log('Server started...'));
+}
 
 function handle(req, res) {
     switch(req.url) {
@@ -17,6 +26,9 @@ function handle(req, res) {
     case '/p5.min.js':
 	serveStatic(req.url.slice(1), res, 'text/javascript');
 	break;
+    case '/data':
+	data(res);
+	break
     default:
 	res.statusCode = 404;
 	res.statusMessage = 'Not found';
@@ -30,4 +42,16 @@ function serveStatic(filename, res, contentType) {
     fs.readFile(filename, 'binary', (_, s) => res.write(s, () => res.end()));
 }
 
-server.listen(PORT, () => console.log('Server started...'));
+function data(res) {
+    res.setHeader('Content-Type', 'application/json');
+    r.table('weather').run(server._rConn)
+	.then(c => c.toArray())
+	.then(o => res.end(JSON.stringify(o)))
+    	.error(e => handleError(res, e));
+}
+
+function handleError(res, err) {
+    res.statusCode = 500;
+    res.statusMessage = 'Internal server error';
+    res.end(err);
+}
