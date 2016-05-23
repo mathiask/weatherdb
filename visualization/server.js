3ac8dfeb2c2a743e3abde3b59e051a27e8c33810
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2016-05-24 02:04:28 +1000

    tiny static file server
    (serving a small p5.js example)

--- visualization/server.js
+++ visualization/server.js
@@ -1,11 +1,33 @@
 'use strict';
 
 const http = require('http'),
+      fs = require('fs'),
       PORT = 80,
       server = http.createServer(handle);
 
 function handle(req, res) {
-    res.end('OK\n');
+    switch(req.url) {
+    case '/ok':
+	res.end('OK\n');
+	break;
+    case '/':
+    case '/index.html':
+	serveStatic('index.html', res, 'text/html');
+	break;
+    case '/p5.min.js':
+	serveStatic(req.url.slice(1), res, 'text/javascript');
+	break;
+    default:
+	res.statusCode = 404;
+	res.statusMessage = 'Not found';
+	res.end();
+	break;
+    }
+}
+
+function serveStatic(filename, res, contentType) {
+    res.setHeader('Content-Type', contentType);
+    fs.readFile(filename, 'binary', (_, s) => res.write(s, () => res.end()));
 }
 
 server.listen(PORT, () => console.log('Server started...'));
