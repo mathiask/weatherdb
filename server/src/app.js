4ee857ccd58b123a5f5c057e2563fc8a848c1d98
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2016-05-25 07:05:25 +1000

    Fixed POST handler in server

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 'use strict';
 
 
-const express = require('express'),
-      r = require('rethinkdb'),
-      app = express();
+const app = require('express')(),
+      bodyParser = require('body-parser'),
+      r = require('rethinkdb');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 r.connect({host:'rethinkdb'})
     .then(rememberConnection)
@@ -38,7 +40,7 @@ function startServer() {
 app.get('/', (_,res) => res.send('Hello, world!\n') );
 
 
-app.post('/temp', (req, res) => recordTemperature(req.query.v, res));
+app.post('/temp', (req, res) => {console.log('##', req.body); recordTemperature(req.body.v, res)});
 
 // GET is only added to allow easy testing
 app.get('/temp/:temp', (req, res) => recordTemperature(req.params.temp, res));
@@ -54,7 +56,10 @@ function recordTemperature(temp, res) {
 }
 
 function handleError(res) {
-    return () => res.send(500, 'Internal error\n');
+    return e => {
+	console.error(e);
+	res.status(500).send('Internal server error\n');
+    };
 }
 
 
