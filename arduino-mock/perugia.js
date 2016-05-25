1d6dca81e094fa3f0eba0a0831bdd4f0b6a01ee7
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2016-05-25 23:14:13 +1000

    implemented reading from openweathermap to send to server

---
+++ arduino-mock/perugia.js
@@ -0,0 +1,41 @@
+// Feed current weather data from Perugia to weatherdb server
+
+const http = require('http'),
+      apikey = require('./apikey'),
+      PerugiaCityId = 3171180;
+
+
+var s='value=hello',
+    options = {
+	host: 'server',
+	port: 3000,
+	path: '/temp',
+	method: 'POST',
+	headers: {
+	    'Content-Type': 'application/x-www-form-urlencoded'
+	}
+    };
+
+
+function readAndSendTemperature() {
+    console.log('Getting current temperature');
+    http.get(`http://api.openweathermap.org/data/2.5/weather?id=${PerugiaCityId}&units=metric&&appid=${apikey.key}`,
+	     res => {
+		 res.setEncoding('utf8');
+		 res.on('data', sendTemperature)
+	     });
+}
+
+function sendTemperature(s) {
+    var data = `v=${JSON.parse(s).main.temp}`,
+	req;
+
+    options.headers['Content-Length'] = data.length;
+    console.log('Sending ', data);
+    req = http.request(options);
+    req.write(data);
+    req.end();
+    setTimeout(readAndSendTemperature, 1000 * 60 * 15);
+}
+
+readAndSendTemperature();
